198f930f71d8bd25698bdf1843f83eeede6e7edf
Henrique Andrade <henrique.portugal15@gmail.com>
2024-12-02 13:24:45 +0000

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,4 @@
+# \r\n
+print(12, 34,1011, sep="-", end="\r##")
+print(56, 78, sep="-", end="\n")
+print(3, 10, sep="-", end="\n")
---
+++ aula2.py
@@ -0,0 +1,9 @@
+concatenacao = "José Henrique" + " " + "Silva Andrade"
+print(concatenacao)
+concatenacao = "1" + "2"
+print(concatenacao)
+
+a_dez_vezes = "A" * 10
+tres_vezes_henrique = 3 * "Henrique"
+print(a_dez_vezes)
+print(tres_vezes_henrique)
